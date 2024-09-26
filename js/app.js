bad2ea4d000e84b2e627c364d7a2eb22f6be8c57
James09977 <zakaria.james1@gmail.com>
2024-09-26 13:03:16 +0600

    search handler

--- js/app.js
+++ js/app.js
@@ -84,7 +84,7 @@ const readHandler = (title, view_count) => {
   markRead.innerText = markReadValue + 1;
   const postContainer = document.getElementById("read-container");
   const div = document.createElement("div");
-  div.classList = `flex justify-around bg-white mx-11 my-4 p-3 rounded-2xl`;
+  div.classList = `flex justify-around mx-11 my-4 p-3 rounded-2xl bg-[#12132d15]`;
   div.innerHTML = `
     <div>
           <p class="text-base font-semibold me-4">
@@ -97,6 +97,94 @@ const readHandler = (title, view_count) => {
        </div>
   `;
   postContainer.appendChild(div);
+};
+
+//handel search
+
+const handleSearch = () => {
+  const searchInput = document.getElementById("search-input");
+  const inputText = searchInput.value;
+  searchInput.value = "";
+  loadSearchData(inputText);
+};
+
+const loadSearchData = async (category) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/retro-forum/posts?category=${category}`
+  );
+  const data = await res.json();
+  const searchItem = data.posts;
+  displaySreachResult(searchItem);
+};
+
+//display search result
+const displaySreachResult = (searchItems) => {
+  const postContainer = document.getElementById("post-container");
+  postContainer.innerText = "";
+  searchItems.forEach((item) => {
+    console.log(item);
+    const card = document.createElement("div");
+    card.classList = `card flex  flex-row  p-10 mb-5 bg-[#797dfc2f]
+    `;
+    card.innerHTML = `
+    <div class="w-20 me-14">
+               <img class="rounded w-16" src="${item.image}" alt="" />
+               <img
+                 class="relative bottom-14 left-8 lg:bottom-20 lg:left-14 hidden"
+                 src="images/online.png"
+                 alt=""
+               />
+               <img
+                 class="relative bottom-14 left-8 lg:bottom-20 lg:left-16"
+                 src="images/offline.png"
+                 alt=""
+               />
+        </div>
+             <div>
+               <div class="flex justify-start font-bold text-sm mb-3">
+                 <p class="me-7"># ${item.category}</p>
+                 <p>Author: <span>${item.author.name}</span></p>
+               </div>
+               <div>
+                 <h4 class="font-bold text- mb-4">
+                   ${item.title}
+                 </h4>
+                 <p class="text-base text-[#12132d8a] mb-5">
+                    ${item.description} is simply dummy text of the printing and typesetting industry. Lorem 
+                 </p>
+                 <hr />
+
+                 <div class="flex justify-between mt-6">
+                   <div class="flex text-[#12132d79]">
+                     <p>
+                       <i class="fa-regular fa-message"></i>
+                       <span class="ps-3 pe-6">${item.comment_count} </span>
+                     </p>
+                     <p>
+                       <i class="fa-regular fa-eye"></i
+                       ><span class="ps-3 pe-6">${item.view_count}</span>
+                     </p>
+                     <p>
+                       <i class="fa-regular fa-clock"></i
+                       ><span class="ps-3 pe-6">${item.posted_time}  min</span>
+                     </p>
+                   </div>
+
+                   <button onclick="readHandler('${item.title}', '${item.view_count}')" class=" w-8 h-8 ">
+                     <i
+                    
+                       class="fa-solid 
+                      
+                       fa-envelope-open"
+                       style="color: #272af5"
+                     ></i>
+                   </button>
+                 </div>
+               </div>
+             </div>
+   `;
+    postContainer.appendChild(card);
+  });
 };
 
 loadData();
